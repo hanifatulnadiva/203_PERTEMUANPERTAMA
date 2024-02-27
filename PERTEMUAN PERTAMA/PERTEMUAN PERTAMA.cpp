0f617a7d58fa205d535755093a20ef032848189c
Hanifatul Nadiva <h.nadiva.ft23@mail.umy.ac.id>
2024-02-27 16:13:04 +0700

    menambahkan perhitungan variabel persegi panjang

--- PERTEMUAN PERTAMA/PERTEMUAN PERTAMA.cpp
+++ PERTEMUAN PERTAMA/PERTEMUAN PERTAMA.cpp
@@ -22,5 +22,14 @@ int main()
     cin >> nTinggi;
     nLuas = nAlas * nTinggi / 2;
     cout << "Luasnya = " << nLuas;
-}
+
+    int  Panjang, Lebar,  Luas; 
+    cout << "Masukan Panjang = ";
+    cin >> Panjang;
+    cout << "Masukan Lebar = ";
+    cin >> Lebar;
+    Luas = Panjang * Lebar;
+    cout << "Luas persegi panjang = " << Luas;
+   
+} 
 
